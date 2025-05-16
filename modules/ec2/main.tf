5168aff28ff08c7662e321450d0688e49df6730c
SathuSid <sisindhriy123@gmail.com>
2025-05-16 21:00:01 +0530

    roboshop terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "sg" {
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = aws_security_group.sg.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = "${var.component_name}-${var.env}"
   }
